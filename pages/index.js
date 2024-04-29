cf36d400396312a9badf6b3a8ce27728057c51db
Ahtisham8952 <ahtisham.badar786@gmail.com>
2024-04-29 19:52:01 +0500

    farmer-motion

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SnapScroll from "../src/components/app/Index";
+
+
+
+
+
+const Home = () => {
+  return (
+    <>
+<SnapScroll/>
+    </>
+  );
+};
+
+export default Home;
